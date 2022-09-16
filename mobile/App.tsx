a76410104ce66912bc17fa0b9ca7bb5134cdb246
kauanfiolo-04 <kauanfiolo@hotmail.com>
2022-09-15 22:40:57 -0300

    Subindo os arquivos mobile q eu tinha esquecido de subir

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,40 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Background } from './src/components/background';
 
-interface ButtonProps{
-  title: string;
-}
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black
+} from '@expo-google-fonts/inter';
+
+import {Home} from './src/screens/Home';
+import { StatusBar } from 'react-native';
+import { Loading } from './src/components/Loading';
 
 export default function App() {
+  const [fontsLoaded]= useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black
+  });
   return (
-    <View style={styles.container}>    
-
-      <Button title="send 1 "/>
-      <Button title="send 2 "/>
-      <Button title="send 3 "/>
-      <Button title="Hello World!"/>
-
-      <StatusBar style="auto" />
-    </View>
+    <Background>
+      <StatusBar
+        barStyle={"light-content"}
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Home />: <Loading />}
+    </Background>
   );
 }
 
 
-function Button(props: ButtonProps){
-  return(
-    <TouchableOpacity>
-      <Text>
-        {props.title}
-      </Text>
-    </TouchableOpacity>
-  )
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
