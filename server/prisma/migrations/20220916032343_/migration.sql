8ad58c09ad9afaa2ff9f263456f39c7e407f3488
kauanfiolo-04 <kauanfiolo@hotmail.com>
2022-09-16 00:37:50 -0300

    Arrumando o back-end pra rodar no meu pc

--- server/prisma/migrations/20220916032343_/migration.sql
+++ server/prisma/migrations/20220916032343_/migration.sql
@@ -1,14 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the `game` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-PRAGMA foreign_keys=off;
-DROP TABLE "game";
-PRAGMA foreign_keys=on;
-
 -- CreateTable
 CREATE TABLE "Game" (
     "id" TEXT NOT NULL PRIMARY KEY,
